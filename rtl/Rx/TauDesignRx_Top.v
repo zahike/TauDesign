2cb2482a07132cdb6b39a967072523587f421daf
zahike <zahike@yahoo.com>
2021-12-10 16:34:33 +0200

    Add SD BOOT.bin for Tx & Rx

--- rtl/Rx/TauDesignRx_Top.v
+++ rtl/Rx/TauDesignRx_Top.v
@@ -23,6 +23,28 @@
 module TauDesignRx_Top(
 input  sysclk,
 
+inout [14:0] DDR_addr,
+inout [2:0] DDR_ba,
+inout  DDR_cas_n,
+inout  DDR_ck_n,
+inout  DDR_ck_p,
+inout  DDR_cke,
+inout  DDR_cs_n,
+inout [3:0] DDR_dm,
+inout [31:0] DDR_dq,
+inout [3:0] DDR_dqs_n,
+inout [3:0] DDR_dqs_p,
+inout  DDR_odt,
+inout  DDR_ras_n,
+inout  DDR_reset_n,
+inout  DDR_we_n,
+inout  FIXED_IO_ddr_vrn,
+inout  FIXED_IO_ddr_vrp,
+inout [53:0] FIXED_IO_mio,
+inout  FIXED_IO_ps_clk,
+inout  FIXED_IO_ps_porb,
+inout  FIXED_IO_ps_srstb,
+
   output      hdmi_tx_clk_n   ,
   output      hdmi_tx_clk_p   ,
   output [2:0]hdmi_tx_data_n  ,
@@ -58,6 +80,29 @@ wire [3:0] CS_n;
 TauDesignRx_BD TauDesignRx_BD_inst
 (
 .sysclk(sysclk),        //input  sysclk
+
+.DDR_addr(DDR_addr),		//inout [14:0] DDR_addr
+.DDR_ba(DDR_ba),		//inout [2:0] DDR_ba
+.DDR_cas_n(DDR_cas_n),		//inout  DDR_cas_n
+.DDR_ck_n(DDR_ck_n),		//inout  DDR_ck_n
+.DDR_ck_p(DDR_ck_p),		//inout  DDR_ck_p
+.DDR_cke(DDR_cke),		//inout  DDR_cke
+.DDR_cs_n(DDR_cs_n),		//inout  DDR_cs_n
+.DDR_dm(DDR_dm),		//inout [3:0] DDR_dm
+.DDR_dq(DDR_dq),		//inout [31:0] DDR_dq
+.DDR_dqs_n(DDR_dqs_n),		//inout [3:0] DDR_dqs_n
+.DDR_dqs_p(DDR_dqs_p),		//inout [3:0] DDR_dqs_p
+.DDR_odt(DDR_odt),		//inout  DDR_odt
+.DDR_ras_n(DDR_ras_n),		//inout  DDR_ras_n
+.DDR_reset_n(DDR_reset_n),		//inout  DDR_reset_n
+.DDR_we_n(DDR_we_n),		//inout  DDR_we_n
+.FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),		//inout  FIXED_IO_ddr_vrn
+.FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),		//inout  FIXED_IO_ddr_vrp
+.FIXED_IO_mio(FIXED_IO_mio),		//inout [53:0] FIXED_IO_mio
+.FIXED_IO_ps_clk(FIXED_IO_ps_clk),		//inout  FIXED_IO_ps_clk
+.FIXED_IO_ps_porb(FIXED_IO_ps_porb),		//inout  FIXED_IO_ps_porb
+.FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),		//inout  FIXED_IO_ps_srstb
+
 .TMDS_Clk_n_0 (hdmi_tx_clk_n ),		//output  TMDS_Clk_n
 .TMDS_Clk_p_0 (hdmi_tx_clk_p ),        //output  TMDS_Clk_p
 .TMDS_Data_n_0(hdmi_tx_data_n),        //output [2:0] TMDS_Data_n
